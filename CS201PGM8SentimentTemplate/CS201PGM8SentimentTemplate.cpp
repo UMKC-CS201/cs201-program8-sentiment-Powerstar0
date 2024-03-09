7528524291170e59c7d86b677364554b56d40a5a
Powerstar0 <133736954+Powerstar0@users.noreply.github.com>
2024-03-08 22:47:20 -0600

    Changes

--- CS201PGM8SentimentTemplate/CS201PGM8SentimentTemplate.cpp
+++ CS201PGM8SentimentTemplate/CS201PGM8SentimentTemplate.cpp
@@ -6,22 +6,37 @@ using namespace std;
 
 int main()
 {
+    srand(time(0));
     vector<wordList> sentList, posList, negList;
 
     // load vectors 
     loadSentiment(sentList, posList, negList);
 
-    // open output file 
+    // open output file
+    ofstream outFile;
+    outFile.open("outputReport.txt");
+    if (!outFile.is_open()) {
+        cout << "Unable to open output file\n";
+        return 1;
+    }
     // open review files
 
     string fileName;
     for (unsigned int i = 1; i < 9; i++) {
         // open input file adding to_string(i) + ".txt" to review
+        string fileName = "review" + to_string(i) + ".txt";
+        ifstream inFile;
+        inFile.open(fileName);
         // if not able to open, print a message and continue
+        if (!inFile.is_open()) {
+            cout << "Error Opening File: " << fileName << endl;
+            continue;
+        }
         // else process the file & close it
-
+        processFile(inFile, outFile, fileName, sentList, posList, negList);
+        inFile.close();
     }
 
     //close the output file
+    outFile.close();
 }
- 
--- CS201PGM8SentimentTemplate/Sentiment.cpp
+++ CS201PGM8SentimentTemplate/Sentiment.cpp
@@ -5,7 +5,10 @@
 //POST:  returns the sentiment of the word if found or the value 0 if not
 double sentimentVal(string in, vector<wordList> &words) {
 
-    cout << "CODE THIS\n";
+    for (auto i : words) {
+        if (i.word == in)
+            return i.value;
+    }
     return 0;
 
 }
@@ -16,14 +19,42 @@ double sentimentVal(string in, vector<wordList> &words) {
 void loadSentiment(vector<wordList>& sentList,
     vector<wordList>& posList,
     vector<wordList>& negList) {
-
+    ifstream inFile;
     //open sentiment.txt
-    //add the word to the sentList vector
-    //if positive enough add to posList
-    //if negative enough add to negList
+    inFile.open("/Users/johnnydiep/CLionProjects/cs201-program8-sentiment-Powerstar0/CS201PGM8SentimentTemplate/sentiment.txt");
+    if (!inFile.is_open()) {
+        cout << "Unable to open input file\n";
+        return;
+    }
+    wordList tempWord;
+    string inRecord;
+    getline(inFile, inRecord);
+    while (inFile.good()) {
+        //add the word to the sentList vector
+        //if positive enough add to posList
+        //if negative enough add to negList
+        try {
+            int pos = inRecord.find(",");
+            tempWord.word = inRecord.substr(0, pos);
+            tempWord.value = stod(inRecord.substr(pos + 1));
+            sentList.push_back(tempWord);
+            if (tempWord.value > 1.5) {
+                posList.push_back(tempWord);
+            }
+            else if (tempWord.value < -1.5) {
+                negList.push_back(tempWord);
+            }
+        }
+        catch (invalid_argument) {
+            cout << "Ignoring: " << tempWord.word << endl;
+        }
+        catch (...) {
+            cout << "Unknown Error - Ignoring : " << tempWord.word << endl;
+        }
+        getline(inFile, inRecord);
+    }
     //close sentiment.txt
-
-    cout << "CODE THIS\n";
+    inFile.close();
     return;
  
 }
@@ -44,11 +75,18 @@ void processFile(ifstream& inFile, ofstream& outFile, string fileName,
     string inWord, newWord;
     char newChar;
     vector<wordList> origWords, negWords, posWords;
+    double origWordsTotal = 0;
+    double negWordsTotal = 0;
+    double posWordsTotal = 0;
 
     cout << "CODE THIS\n";
     outFile << "PROCESSING FILE: " << fileName << endl << endl;
 
     // reset all vectors & values before processing inFile
+    origWords.clear();
+    negWords.clear();
+    posWords.clear();
+
     //   read and edit the initial file
     //   remove any unnecessary characters
     while (inFile >> inWord) {
@@ -63,14 +101,42 @@ void processFile(ifstream& inFile, ofstream& outFile, string fileName,
                 newWord += newChar;
             }
         }
+        cout << newWord << " : " << sentimentVal(newWord, words) << endl;
         // add the 'clean' word to a temp location & add original word & sentiment value to origWords
-
+        string clean_word = newWord;
+        wordList tempWord;
+        tempWord.word = newWord;
+        tempWord.value = sentimentVal(newWord, words);
+        origWordsTotal += tempWord.value;
+        origWords.push_back(tempWord);
         //CHECK TO MAKE SENTIMENT MORE NEGATIVE
+        if (tempWord.value > 1) {
+            srand(time(0));
+            int random = rand() % negList.size();
+            wordList random_word_neg = negList[random];
+            negWords.push_back(random_word_neg);
+            negWordsTotal += random_word_neg.value;
+        }
+        else {
+            negWords.push_back(tempWord);
+            negWordsTotal += tempWord.value;
+        }
         //store current word before checking to replace
         //add original word or replacement to negVector
 
 
         //CHECK TO MAKE SENTIMENT MORE POSITIVE
+        if (tempWord.value < 1) {
+            srand(time(0));
+            int random = rand() % posList.size();
+            wordList random_word_pos = posList[random];
+            posWords.push_back(random_word_pos);
+            posWordsTotal += random_word_pos.value;
+        }
+        else {
+            posWords.push_back(tempWord);
+            posWordsTotal += tempWord.value;
+        }
         //store current word before checking to replace
         //add original word or replacement to posVector
     }
@@ -87,16 +153,65 @@ void processFile(ifstream& inFile, ofstream& outFile, string fileName,
         }
         outFile << origWords[i].word + " ";
     }
- 
+    outFile << endl << endl;
+    outFile << "ORIGINAL SENTIMENT: " << origWordsTotal;
+    outFile << endl << endl;
 
     //EDIT MORE NEGATIVE VECTOR FOR OUTPUT
+    outFile << "UPDATED REVIEW (MORE NEGATIVE): " << endl;
+    for (unsigned int i = 0; i < negWords.size(); i++) {
+        lineLength += negWords[i].word.length() + 1;
+        if (lineLength > 80) {
+            outFile << "\n";
+            lineLength = negWords[i].word.length() + 1;
+        }
+        outFile << negWords[i].word + " ";
+    }
     //CHECK THAT SOMETHING HAS BEEN UPDATED BEFORE BEGINNING PRINT
     //IF THE ORIGINAL SENTMENT & NEGATIVE SENTIMENT ARE EQUAL, PRINT "REVIEW NOT UPDATED, THE SENTIMENT REMAINS: "
+    outFile << endl << endl;
+    outFile << "NEGATIVE SENTIMENT: " << negWordsTotal;
+    outFile << endl << endl;
 
 
     //EDIT MORE POSITIVE VECTOR FOR OUTPUT
+    outFile << "UPDATED REVIEW (MORE POSITIVE): " << endl;
+    for (unsigned int i = 0; i < posWords.size(); i++) {
+        lineLength += posWords[i].word.length() + 1;
+        if (lineLength > 80) {
+            outFile << "\n";
+            lineLength = posWords[i].word.length() + 1;
+        }
+        outFile << posWords[i].word + " ";
+    }
     //CHECK THAT SOMETHING HAS BEEN UPDATED BEFORE BEGINNING PRINT
     //IF THE ORIGINAL SENTMENT & NEW POSITIVE SENTIMENT ARE EQUAL, PRINT "REVIEW NOT UPDATED, THE SENTIMENT REMAINS: "
+    outFile << endl << endl;
+    outFile << "POSITIVE SENTIMENT: " << posWordsTotal;
+    outFile << endl << endl << endl;
  
  
+}
+
+void processFile(ifstream inFile, ofstream outFile, vector<wordList> sentWordList, vector<wordList> posList, vector<wordList> negList) {
+    /* SETUP inFileName as ifstream
+     * WHILE (inFILE >> origWord)
+     *  LOOP through origWord and change uppercase alpha to lowercase
+     *  SET editWord to this updated word
+     *  SET wordValue = sentimentVal(editWord)
+     *  UPDATE totalOrigSent += Value
+     *  ADD word, wordValue to vector
+     *  IF (wordValue > 1)
+     *      GET randomNegWord
+     *      ADD randomNegWord to negWords
+     *  ELSE
+     *      ADD origWord to negWords
+     *  IF (wordValue < -1)
+     *      GET randomPosWord
+     *      ADD randomPosWord & value to posWords
+     *  ELSE
+     *      ADD origWord to posWords
+     * ENDWHILE
+     * PRINT vectors
+     * */
 }
